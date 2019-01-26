ff540cbac6006f31a5e68f321182f6562a8b1b44
Ilya Zarudny <zaruda46@gmail.com>
2019-01-26 14:52:13 +0300

    normalization of slugs

--- app/models/product_type.rb
+++ app/models/product_type.rb
@@ -2,6 +2,10 @@ class ProductType < ApplicationRecord
   extend FriendlyId
   friendly_id :name, use: :slugged
 
+  def normalize_friendly_id(input)
+    input.to_s.to_slug.normalize(transliterations: :russian).to_s
+  end
+
   has_many :products, dependent: :destroy
   has_one_attached :image
 end
