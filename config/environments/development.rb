51a3264bbdfed9dec09e2bd763b56fc45f492541
ilyazarudny <zaruda46@gmail.com>
2019-05-13 16:46:12 +0300

    revert cdn

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,8 +34,6 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: "locahost:3000" }
-
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: ENV['SMTP_SERVER'],
