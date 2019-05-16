2e7ebdeb041de3ea0602254c5d6aa46319bd1945
ilyazarudny <zaruda46@gmail.com>
2019-05-16 13:29:54 +0300

    added necessary titles

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,6 +35,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.delivery_method = :smtp
+
   config.action_mailer.smtp_settings = {
     address: ENV['SMTP_SERVER'],
     port: ENV['SMTP_PORT'],
