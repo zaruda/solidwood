68770b252d274f1e356c9d5a7e3d8dbdd84d4d34
ilyazarudny <zaruda46@gmail.com>
2020-01-16 23:55:06 +0300

    fix

--- lib/builders/Breadcrumbs.rb
+++ lib/builders/Breadcrumbs.rb
@@ -1,14 +1,16 @@
-module Breadcrumbs
-    class MicrodataBuilder < BreadcrumbsOnRails::Breadcrumbs::Builder
-      def render
-        @elements.collect(&method(:render_element)).join(@options[:separator] || " &raquo; ")
-      end
+require 'breadcrumbs_on_rails'
 
-      def render_element(element)
-        url = (compute_path(element).present? ? compute_path(element) : '#')
-        subcontent = @context.content_tag(:span, compute_name(element), :itemprop => 'name')
-        content = @context.link_to_unless_current(subcontent, url, :itemprop => 'item')
-        @context.content_tag(:li, content, itemscope: "", itemprop: "itemListElement", itemtype: "http://schema.org/ListItem")
-      end
+module Breadcrumbs
+  class MicrodataBuilder < BreadcrumbsOnRails::Breadcrumbs::Builder
+    def render
+      @elements.collect(&method(:render_element)).join(@options[:separator] || " &raquo; ")
     end
+
+    def render_element(element)
+      url = (compute_path(element).present? ? compute_path(element) : '#')
+      subcontent = @context.content_tag(:span, compute_name(element), :itemprop => 'name')
+      content = @context.link_to_unless_current(subcontent, url, :itemprop => 'item')
+      @context.content_tag(:li, content, itemscope: "", itemprop: "itemListElement", itemtype: "http://schema.org/ListItem")
+    end
+  end
 end
