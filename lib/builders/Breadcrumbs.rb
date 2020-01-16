a79f9e4f2fcf97ca4e459920fe205fb28f9463ef
ilyazarudny <zaruda46@gmail.com>
2020-01-16 23:41:11 +0300

    add missed builder

---
+++ lib/builders/Breadcrumbs.rb
@@ -0,0 +1,14 @@
+module Breadcrumbs
+    class MicrodataBuilder < BreadcrumbsOnRails::Breadcrumbs::Builder
+      def render
+        @elements.collect(&method(:render_element)).join(@options[:separator] || " &raquo; ")
+      end
+
+      def render_element(element)
+        url = (compute_path(element).present? ? compute_path(element) : '#')
+        subcontent = @context.content_tag(:span, compute_name(element), :itemprop => 'name')
+        content = @context.link_to_unless_current(subcontent, url, :itemprop => 'item')
+        @context.content_tag(:li, content, itemscope: "", itemprop: "itemListElement", itemtype: "http://schema.org/ListItem")
+      end
+    end
+end
