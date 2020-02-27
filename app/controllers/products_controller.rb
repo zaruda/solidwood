9f09adbba3c743d3c847134a96aec20cb4fb7dee
Ilya Zarudny <zaruda46@gmail.com>
2020-02-27 11:54:58 +0300

    Hide inactive products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,12 +3,13 @@ class ProductsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
-    @products = ProductType.all
+    @products = ProductType.active
     @order_item = current_order.order_items.new
   end
 
   def show
     @product_type = ProductType.friendly.find(params[:id])
+    redirect_to '/catalog/product', status: 301 unless @product_type.active
     @order_item = current_order.order_items.new
   end
 
