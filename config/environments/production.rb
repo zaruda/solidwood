a07e0e91af2dac36553c81665856ff73d4abb69f
Ilya Zarudny <zaruda46@gmail.com>
2023-03-26 23:54:00 +0300

    Try to fix smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,6 +101,8 @@ Rails.application.configure do
       user_name: ENV['SMTP_LOGIN'],
       password: ENV['SMTP_PASSWORD'],
       enable_starttls_auto: true,
+      tls: true,
+      ssl: true,
   }
 
 
